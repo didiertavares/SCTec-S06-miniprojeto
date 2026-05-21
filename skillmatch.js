f932287fbe39945018b7f711067c91c08fb2c86b
Didier Emmanuel TAVARES <didier.tavares@gmail.com>
2026-05-21 18:59:31 -0300

    em andamento: tentativas, erros & acertos: ajustando métodos funções e loops

--- skillmatch.js
+++ skillmatch.js
@@ -23,11 +23,11 @@ const candidatos = []
 
 // CRIAÇÃO DE UM OBJETO CANDIDATO a partir de sua classe
 // -> inserção desse candidato no array geral (base RH) de candidatos disponiveis
-candidatos.push(new Candidato('Tânia FONSECA', 'FrontEnd', ['JavaScript', 'HTML', 'CSS', 'Github', 'lógica de programação', 'Kanban'], 26, this.nivel))
+candidatos.push(new Candidato('Tânia FONSECA', 'FrontEnd', ['JavaScript', 'HTML', 'CSS', 'Github', 'lógica de programação', 'Kanban', 'DevTools', 'React'], 26, this.nivel))
 // console.log(candidatos)
 
 
-// APRESENTAÇÃO DO(S) CANDIpadrão DATO(S)
+// APRESENTAÇÃO DO(S) CANDIDATO(S)
 candidatos.forEach(candidato =>{
     console.log('Nome do(a) candidato(a): ' + candidato.nome +
     '\nÁrea: ' + candidato.area +
@@ -37,17 +37,7 @@ candidatos.forEach(candidato =>{
 })
 
 
-
-
-// //  CRIAÇÃO DE ARRAY EXCLUSIVO DAS HABILIDADES DO CANDIDATO
-// const skillsCandidato = [candidato.habilidades]  /*.map(elemento => elemento.toString().toLowerCase())*/
-// console.log(skillsCandidato)
-
-
-
 // const skillsFormatdLowerCase = skillsCandidato.map(elemento => elemento.toString().toLowerCase())
-// console.log(skillsFormatdLowerCase[1])
-// console.log(skillsFormatdLowerCase)
 
 
 
@@ -79,106 +69,108 @@ class VagaDev extends Vaga{
 
 
 // CONSTRUÇÃO DO ARRAY DE OBJETOS-VAGAS A PARTIR DA CLASSE VAGADEV
-const vagaDevIndexadas = []
+const vagasIndex = []
 
-vagaDevIndexadas.push(new VagaDev(1, 'Desenvolvedor Front-End', 'StormCode', ['JavaScript', 'Github', 'lógica de programação'], 'Remoto', 2800, 'Junior'))
+vagasIndex.push(new VagaDev(1, 'Desenvolvedor Front-End', 'StormCode', ['JavaScript', 'Github', 'lógica de programação'], 'Remoto', 2800, 'Junior'))
 
-vagaDevIndexadas.push(new VagaDev(2, 'Estagiário Front-End', 'TechRockode', ['JavaScript', 'Github', 'lógica de programação', 'Kanban'], 'Híbrido', 1800, "Estágio"))
+vagasIndex.push(new VagaDev(2, 'Estagiário Front-End', 'TechRockode', ['JavaScript', 'Github', 'lógica de programação', 'Kanban'], 'Híbrido', 1800, "Estágio"))
 
-vagaDevIndexadas.push(new VagaDev(3, 'Programador Javascript', 'GoBuzz Lab', ['JavaScript', 'arrays', 'objetos', 'funções', 'Github', 'lógica de programação'], 'Presencial', 3000, "Junior"))
+vagasIndex.push(new VagaDev(3, 'Programador Javascript', 'GoBuzz Lab', ['JavaScript', 'arrays', 'objetos', 'funções', 'Github', 'lógica de programação'], 'Presencial', 3000, "Junior"))
 
-vagaDevIndexadas.push(new VagaDev(4, "Desenvolvedor Front-End", "WebStart Tecnologia", ["JavaScript", "HTML", "CSS", 'Github', 'lógica de programação', "React", "Git"], "Remoto", 4500, "Junior"))
+vagasIndex.push(new VagaDev(4, "Desenvolvedor Front-End", "WebStart Tecnologia", ["JavaScript", "HTML", "CSS", 'Github', 'lógica de programação', "React", "Git"], "Remoto", 4500, "Junior"))
 
-vagaDevIndexadas.push(new VagaDev(5, "Estagiário em Desenvolvimento Web", "Inova Digital", ['Github', 'lógica de programação', "HTML", "CSS", "JavaScript", "Bootstrap"], "Híbrido", 1800, "Estágio"))
+vagasIndex.push(new VagaDev(5, "Estagiário em Desenvolvimento Web", "Inova Digital", ['Github', 'lógica de programação', "HTML", "CSS", "JavaScript", "Bootstrap"], "Híbrido", 1800, "Estágio"))
 
-vagaDevIndexadas.push(new VagaDev(6, "Front-End Developer (React) - Pleno", "Fintech Nexus", ["JavaScript", 'Github', 'lógica de programação', "TypeScript", "React", "Next.js", "Tailwind CSS"], "Remoto", 8500, "Pleno"))
+vagasIndex.push(new VagaDev(6, "Front-End Developer (React) - Pleno", "Fintech Nexus", ["JavaScript", 'Github', 'lógica de programação', "TypeScript", "React", "Next.js", "Tailwind CSS"], "Remoto", 8500, "Pleno"))
 
-vagaDevIndexadas.push(new VagaDev(7, "Desenvolvedor Full Stack JS", "Core Systems", ["JavaScript", 'Github', 'lógica de programação', "Node.js", "Express", "React", "MongoDB"], "Presencial", 5000, "Junior"))
+vagasIndex.push(new VagaDev(7, "Desenvolvedor Full Stack JS", "Core Systems", ["JavaScript", 'Github', 'lógica de programação', "Node.js", "Express", "React", "MongoDB"], "Presencial", 5000, "Junior"))
 
-vagaDevIndexadas.push(new VagaDev(8, "Desenvolvedor Front-End focado em UI", "Creative Studio", ['Github', 'lógica de programação', "HTML", "CSS", "JavaScript", "Sass", "Figma"], "Remoto", 4000, "Junior"))
+vagasIndex.push(new VagaDev(8, "Desenvolvedor Front-End focado em UI", "Creative Studio", ['Github', 'lógica de programação', "HTML", "CSS", "JavaScript", "Sass", "Figma"], "Remoto", 4000, "Junior"))
 
-vagaDevIndexadas.push(new VagaDev(9, "Engenheiro de Software React", "BigTech Brasil", ["JavaScript", 'Github', 'lógica de programação', "TypeScript", "React", "Redux", "Jest", "AWS"], "Remoto", 15000, "Sênior"))
+vagasIndex.push(new VagaDev(9, "Engenheiro de Software React", "BigTech Brasil", ["JavaScript", 'Github', 'lógica de programação', "TypeScript", "React", "Redux", "Jest", "AWS"], "Remoto", 15000, "Sênior"))
 
-vagaDevIndexadas.push(new VagaDev(10, "Vaga Afirmativa - Dev Front-End", "Inclusiva Tech", ['Github', 'lógica de programação', "JavaScript", "HTML", "CSS", "React"], "Remoto", 4200, "Junior"))
+vagasIndex.push(new VagaDev(10, "Vaga Afirmativa - Dev Front-End", "Inclusiva Tech", ['Github', 'lógica de programação', "JavaScript", "HTML", "CSS", "React"], "Remoto", 4200, "Junior"))
 
-vagaDevIndexadas.push(new VagaDev(11, "Desenvolvedor JavaScript (Node.js)", "LogiTech", ['Github', 'lógica de programação', "JavaScript", "Node.js", "TypeScript", "PostgreSQL", "Docker"], "Híbrido", 9000, "Pleno"))
+vagasIndex.push(new VagaDev(11, "Desenvolvedor JavaScript (Node.js)", "LogiTech", ['Github', 'lógica de programação', "JavaScript", "Node.js", "TypeScript", "PostgreSQL", "Docker"], "Híbrido", 9000, "Pleno"))
 
-vagaDevIndexadas.push(new VagaDev(12, "Suporte Técnico Nível 2 / Dev Bug Hunter", "SaaS Global", ['Github', 'lógica de programação', "JavaScript", "HTML", "SQL", "DevTools"], "Presencial", 3500, "Junior"))
+vagasIndex.push(new VagaDev(12, "Suporte Técnico Nível 2 / Dev Bug Hunter", "SaaS Global", ['Github', 'lógica de programação', "JavaScript", "HTML", "SQL", "DevTools"], "Presencial", 3500, "Junior"))
 
-vagaDevIndexadas.push(new VagaDev(13, "Desenvolvedor Front-End Mobile (React Native)", "AppNation", ['Github', 'lógica de programação', "JavaScript", "TypeScript", "React Native", "Android Studio", "iOS"], "Remoto", 9500, "Pleno"))
+vagasIndex.push(new VagaDev(13, "Desenvolvedor Front-End Mobile (React Native)", "AppNation", ['Github', 'lógica de programação', "JavaScript", "TypeScript", "React Native", "Android Studio", "iOS"], "Remoto", 9500, "Pleno"))
 // console.log(vagaDevIndexadas)
 
 
-// APRESENTAÇÃO DAS VAGAS INDEXADAS (SUPOSTAMENTE PELO BUSCADOR)
-vagaDevIndexadas.forEach(item =>{
+// APRESENTAÇÃO FORMATADA DAS VAGAS INDEXADAS (SUPOSTAMENTE PELO BUSCADOR)
+vagasIndex.forEach(item =>{
     console.log(`${item.id}. ${item.empresa} -> ${item.titulo}, nível ${item.nivel}\nRequisitos: ${item.requisitos}\nSalario: R$ ${item.salario}\nModo: ${item.modalidade}\n`)
 })
+
+// // CRIAÇÃO DE ARRAY COMPOSTO EXCLUSIVAMENTE DOS ARRAYS DE TECHS REQUISITADAS NAS VAGAS
+const vagasIndexRequisitos = vagasIndex.map(vaga => ({
+    requisitos: vaga.requisitos
+}))
+console.log(vagasIndexRequisitos)
+
+// CRIAÇÃO DE ARRAY DAS HABILIDADES DO CANDIDATO[0]
+const techsCandidato0 = candidatos[0].habilidades
+console.log(techsCandidato0)
+
+const vagas05Requisitos = vagasIndexRequisitos[5]
+console.log(vagas05Requisitos)
+
+// console.log(
+//     vagasIndex.filter(item => item.requisitos.includes('HTML'))
+// )
+
 // funciona
-// console.log(vagaDevIndexadas[2].requisitos[5])
+// console.log(
+//     vagasIndex.filter(item => item.requisitos.includes('React'))
+// )
 
-// tbm funciona
-// let skillCandidato
 
-// detecção de vagas compatíveis
-// for (i=0; i < candidatos[0].habilidades.length; i++) {
-//         console.log(i)
-// } 
+// ADERÊNCIA DO PERFIL CANDIDATO À CADA VAGA:
+function calcularCompatVaga() {
+    let pontosTotais = 0
+    let pontosGanhos = 0
 
-// for(i=0; i < vagaDevIndexadas.length; i++){
-//     console.log(i)
-// }
-// vagaDevIndexadas.filter(vaga => vaga.requisitos.includes(candidatos[0].habilidades[i]))
+    // compatibilidade em nível de experiência
+    pontosTotais += 2
+    if(candidatos[0].nivel === vagasIndex[2].nivel){pontosGanhos += 2}
+    else {console.log('nível de perfil incompatível')}
+    console.log(pontosGanhos, pontosTotais)
 
-function loopSkillsCandidato(){
-    candidatos[0].habilidades.forEach(habilidade => {
-        // console.log(habilidade)
-        return habilidade
-    })
+    // compatibilidade entre habilidades-Candidato e requisitos-Vaga
+    // vagas05Requisitos.forEach(item => {
+    //     pontosTotais++
+    //     if (techsCandidato0.includes(item)) pontosGanhos++
+    // })
+    // console.log(pontosGanhos, pontosTotais)
 }
-// loopSkillsCandidato()
+calcularCompatVaga()
 
-// vagaDevIndexadas.forEach(vaga => {
-//     vaga.requisitos.filter(requisito => {
-//         requisito === loopSkillsCandidato()? console.log('coincide'): console.log('não coincide');
+vagasIndexRequisitos[5].forEach(item => {
+    console.log(item)
+    // if (techsCandidato0.includes(item)) {pontosGanhos++}
+    // pontosTotais++
+})
+// console.log(pontosGanhos, pontosTotais)
 
-//     })
-// })
-// console.log(candidatos[0].habilidades[1])
-
-console.log(
-    vagaDevIndexadas.filter(item =>
-        item.requisitos.includes('HTML')
-)
-)
-// funciona
-console.log(
-    vagaDevIndexadas.filter(item => item.requisitos.includes('React'))
-)
-
-
-// function requisitosCumpridos(){
-//     for(i=0; i < vagaDevIndexadas.length; i++) {
-//     vagaDevIndexadas[i].requisitos
-//     }        
+// calcularCompatVaga(){
+// for(i=0; i < vagasIndexRequisitos.length; i++){
+//     pontosTotais=2
+//     console.log('teste')
+    // if (vagasIndexRequisitos[i].includes(candidatos[0].habilidades[0])) pontosGanhos++
 // }
 
-// requisitosCumpridos()
 
+// vagasIndex.forEach(vaga => {
+//     pontosTotais
+//     if (vaga.requisitos.includes(candidatos[0].habilidades[7])) 
+//         {console.log(vaga)}
+//     // vaga.filter(vaga.requisitos.includes(candidatos[0].habilidades)
+//         // requisito === loopSkillsCandidato()? : ;
 
-
-
-const listaVagasCompativeis = []
-
-let pontosTotais = 0
-let pontosGanhos = 0
-
-// techsVagaDev01.forEach(item => {
-//     pontosTotais += 1;
-//     if (skillsCandidato.includes(item)) {
-//       pontosGanhos += 1;
-//     }
-//     console.log(pontosTotais + ' | ' + pontosGanhos)
 // })
+
 
 
 
@@ -188,7 +180,7 @@ let pontosGanhos = 0
 // function buscarVagasIndexadas(){
 //     return new Promise((resolve) => {
 //         setTimeout(()=>{
-//             resolve(vagaDevIndexadas);
+//             resolve(vagasIndex);
 //         }, 1000);        
 //     })
 // }
@@ -197,4 +189,3 @@ let pontosGanhos = 0
 //     const vagasCarregadas = await buscarVagasIndexadas();
 //     console.log('Vagas carregadas com sucesso!');
 //     console.log(vagasCarregadas)
-// }
